404b2fa47002e1925010ddcd95cbeda96d6cc2e9
habibaahmeddd <123377854+habibaahmeddd@users.noreply.github.com>
2023-10-27 14:33:42 +0200

    problem4 done

---
+++ A2-S21-20220102-problem4.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void SieveOfEratosthenes(int n)
+{
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p]) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            cout << p << " ";
+}
+
+// Driver Code
+int main() {
+    int n;
+    cin>>n;
+    cout  << n << endl;
+    SieveOfEratosthenes(n);
+}
